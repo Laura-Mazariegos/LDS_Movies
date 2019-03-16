1762087f7d25e8ed7bb7c693975146422ccdb940
laura.mazariegos <35360118+Laura-Mazariegos@users.noreply.github.com>
2019-03-16 15:00:21 -0500

    Add files via upload

---
+++ Data/MvcMovie1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie1.Models;
+
+namespace MvcMovie1.Models
+{
+    public class MvcMovie1Context : DbContext
+    {
+        public MvcMovie1Context (DbContextOptions<MvcMovie1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie1.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Models/MovieGenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie1.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies;
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+        public string DateSort { get; set; }
+        public string MovieSort { get; set; }
+        public string CurrentSort { get; set; }
+        public string CurrentFilter { get; set; }
+    }
+}
